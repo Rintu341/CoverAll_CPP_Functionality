5efe9fa748e342840ceb7746a99ad5b5fdcd1476
sujan <sujanrintu@gmail.com>
2023-07-15 01:25:44 +0530

    add assignment 23 (basic of c++)

---
+++ Basics_of_C++_Assignment23/Question_04.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    float x;
+    cout<<"Enter Rdious of a circle: ";
+    cin>>x;
+    float area;
+    area = 3.142*x*x;
+    cout<<"Area of a circle = "<<area<<endl;
+    
+}
